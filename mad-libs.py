0715e4cbc62138235d334650b88a53f5d16aed37
hdannyyi <hdannyyi@gmail.com>
2019-04-16 15:17:34 -0400

    added a madlibs file

---
+++ mad-libs.py
@@ -0,0 +1,6 @@
+Adjective = input('Please provide an adjective:')
+Name = input('Please provide a name:')
+Dance = input('Please provide a type of dance:')
+
+
+print ('The %s %s likes to %s' % (Adjective, Name, Dance))
