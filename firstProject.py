370ac47b0c6e47331b39e6bd72b10cd2e190aa5c
hdannyyi <hdannyyi@gmail.com>
2019-04-16 15:05:36 -0400

    added inputs

---
+++ firstProject.py
@@ -0,0 +1,9 @@
+FirstName = input('What is your first name?')
+LastName = input('What is your last name?')
+FavMovie = input('What is your favorite movie?')
+
+#print ("First Name: %s" % FirstName) 
+#print ("Last Name: %s" % LastName) 
+#print ("Favorite Movie: %s" % FavMovie)
+
+print ("Hello %s %s. I like %s, too" % (FirstName, LastName, FavMovie)) 
