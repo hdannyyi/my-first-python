43acd70fb535e93415e51380b91518a3295c7384
hdannyyi <hdannyyi@gmail.com>
2019-04-17 15:20:37 -0400

    while loop 1 & 2 exercises

--- while-loop.py
+++ while-loop.py
@@ -1,10 +1,34 @@
 #Prompt user over and over and over again
 
 
-should_run = True
+"""should_run = True
 
 while should_run:
     user_input = input("What? ")
     print("%s" % user_input)
     if user_input == "bye":
-        should_run = False
+        should_run = False"""
+
+# while loop exercises
+
+#1 to 10
+"""count = 0
+
+while count < 10:
+    count = count + 1
+    print(count)"""
+
+#n to m
+"""count = 1
+
+while count < 8:
+    count = count + 1
+    print(count)"""
+
+#odd numbers
+count = 0
+
+while count < 10:
+    count = count + 1
+    if count%2 == 1:
+        print(count)
--- while-loop2.py
+++ while-loop2.py
@@ -3,7 +3,7 @@
 
 #set our bye_count to 0, no one has said anything yet
 
-bye_count = 0
+"""bye_count = 0
 
 while bye_count < 3:
     should_run = True
@@ -15,4 +15,23 @@ while bye_count < 3:
             should_run = False
 
             bye_count = bye_count + 1
-        print(bye_count)
+        print(bye_count)"""
+
+
+#How many coins?
+
+coin_count = 0
+should_run = True
+
+while should_run:
+    print('You have %d coins.' % (coin_count))
+    user_input = input('Do you want another?')
+    if user_input == "yes":
+        coin_count = coin_count + 1
+    elif user_input == "no":
+        should_run = False
+        print("bye")
+
+    
+        
+        
