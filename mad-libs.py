9116b65668dc3a0e112ce495fbb20686636e110a
hdannyyi <hdannyyi@gmail.com>
2019-04-17 12:00:36 -0400

    function form

--- mad-libs.py
+++ mad-libs.py
@@ -1,6 +1,13 @@
-Adjective = input('Please provide an adjective:')
-Name = input('Please provide a name:')
-Dance = input('Please provide a type of dance:')
+Adjective = input('Provide an adjective:')
+Name = input('Provide a name:')
+Dance = input('Provide a type of dance:')
 
+def capitalize(Name):
+    fixed_name = Name.capitalize()
+    return fixed_name
 
-print ('The %s %s likes to %s' % (Adjective, Name, Dance))
+def madlibs(Adjective, Name, Dance):
+    fixed_name = capitalize(Name)
+    return print ('The %s %s likes to %s' % (Adjective, fixed_name, Dance))
+    
+madlibs(Adjective, Name, Dance)
