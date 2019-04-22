59ba967899d4935bbec8621655abb59a610a0218
hdannyyi <hdannyyi@gmail.com>
2019-04-22 14:38:12 -0400

    list exercises and print banner

---
+++ list-exercises.py
@@ -0,0 +1,54 @@
+#List exercises
+
+##Sum the numbers
+
+list_of_numbers = [3151, -12, 834, 3, 9205]
+
+"""for numbers in list_of_numbers:
+    total = sum(list_of_numbers)
+print(total)"""
+
+##Largest number
+"""print (max(list_of_numbers))"""
+
+
+##Smallest number
+"""print (min(list_of_numbers))"""
+
+
+##Even numbers
+
+"""for even in list_of_numbers:
+    if even % 2 == 0:
+        print(even)"""
+
+
+##Positive numbers
+"""for positive in list_of_numbers:
+    if positive > 0:
+        print(positive)"""
+
+
+##Positive numbers 2
+list_of_numbers2 = []
+"""for positive in list_of_numbers:
+    if positive > 0:
+        list_of_numbers2.append(positive)
+print(list_of_numbers2)"""
+
+
+##Multiply a list
+"""factor = 2
+for multiply in list_of_numbers:
+    multiply = multiply * factor
+    list_of_numbers2.append(multiply)
+print(list_of_numbers2)"""
+
+
+##Multiply vectors
+list_of_numbers3 = [3, 7, 17, 94, -1]
+i = 0
+for lup in list_of_numbers:
+    vectors = list_of_numbers[0] * list_of_numbers3 [0]
+    list_of_numbers2.append(vectors)
+print(list_of_numbers2) 
--- print-banner.py
+++ print-banner.py
@@ -4,3 +4,7 @@ stars = '*'
 user_input = input('Text? ')
 
 while banner < 3:
+    print('**' + (stars * len(user_input) + '**'))
+    print(('* ' + user_input + ' *'))
+    print('**' + (stars * len(user_input) + '**'))
+    banner += 1 
